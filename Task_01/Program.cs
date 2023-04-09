a17802784de4cfa8fa834440e7057f8e7eb5c2d0
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-09 16:14:07 +0300

    task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double y = double.Parse(Console.ReadLine());
+
+if(x == Math.Pow(y, 2))
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
