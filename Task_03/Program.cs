a6dd659c4a015a33a79c367637cfe70a9921536e
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-10 10:49:26 +0300

    Task_03.1

---
+++ Task_03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести все целые числа от -N до N
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Ряд данных: ");
+for (int j = -i; j < i+1; j++)
+{
+Console.WriteLine(j);
+}
