ec23457ae39501f1cbea65126de7159f90e8dae2
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-10 16:31:52 +0300

    HW_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if(x % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+} 
+else
+{
+    Console.WriteLine("Число нечетное");
+}
