1acdfa1259f666302ea9259bc4c5c1924995f65b
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-10 14:23:05 +0300

    HW_01

---
+++ HW_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Максимальное и минимальное из 2 чисел
+
+Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = int.Parse(Console.ReadLine());
+
+if(x > y)
+{
+    Console.WriteLine($"Максимальное число: {x}");
+    Console.WriteLine($"Минимальное число: {y}");
+}
+else{
+    Console.WriteLine($"Максимальное число: {y}");
+    Console.WriteLine($"Минимальное число: {x}");
+}
--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,6 +1,14 @@
 ﻿// Вывод последней цифры
 
+Console.Clear();
 Console.Write("Введите число: ");
 int i = int.Parse(Console.ReadLine());
-Console.Write("Последняя цифра: ");
-Console.WriteLine(i % 10);
+//Console.Write("Последняя цифра: ");
+// Console.WriteLine(i % 10);
+if(i >=100 && i <1000)
+{
+    Console.WriteLine($"Последняя цифра: {i % 10}");
+}
+else{
+    Console.WriteLine("Неверное условие");
+}
